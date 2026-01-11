8aa880b461d744a95700dd8acf57fc64145f8d4e
aishudavid06 <aishu.david06@gmail.com>
2026-01-11 18:50:20 +0530

    First commit – Added Java program

---
+++ DA1.java
@@ -0,0 +1,7 @@
+public class DA1 {
+    public static void main(String[] args) {
+        System.out.println("Student Name: AISHWARIYA E");
+        System.out.println("Register Number: 23MIC0081");
+        System.out.println("Message: Jenkins–GitHub Integration Successful");
+    }
+}
